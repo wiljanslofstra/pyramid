ecbc5b15468e2f18825ac213959288da734db43a
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-01-28 12:26:08 +0100

    Move config files and the Webpack compiler script for cleanup 💥

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,18 @@
-const config = require('../config');
 const server = require('../server/main');
-const debug = require('debug')('app:bin:server');
+const debug = require('debug')('app:server');
+const webpack = require('webpack');
+const webpackConfig = require('../build/webpack.config');
+const config = require('../config');
 
-const port = config.server_port;
+const compiler = webpack(webpackConfig);
 
-server.listen(port);
-debug(`Server is now running at http://localhost:${port}.`);
+compiler.watch({ // watch options:
+  aggregateTimeout: 300, // wait so long for more changes
+  poll: true // use polling instead of native watchers
+}, (err, stats) => {
+  if (err) {
+    throw new Error(err);
+  }
+
+  console.log(stats.toString(webpackConfig.stats));
+});
