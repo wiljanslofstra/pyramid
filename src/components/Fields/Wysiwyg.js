15b2e649e8d3bfeda3f30e02e5e0265f50205bf9
Wiljan Slofstra <wiljan@orangetalent.nl>
2017-01-29 20:31:01 +0100

    Very small performance improvement to prevent unnecessary update

--- src/components/Fields/Wysiwyg.js
+++ src/components/Fields/Wysiwyg.js
@@ -1,22 +1,37 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import TinyMCE from 'react-tinymce';
 import getTinyMCEConfig from '../../helpers/getTinyMCEConfig';
 
-const Wysiwyg = ({ data, field, onChange, id }) => (
-  <div className="PyramidBlock__ContentGroup">
-    <label className="PyramidLabel" htmlFor={id}>
-      {field.label}
-    </label>
+class Wysiwyg extends Component {
+  shouldComponentUpdate(nextProps) {
+    // Only update if the data or the id changed
+    if (this.props.data === nextProps.data && this.props.id === nextProps.id) {
+      return false;
+    }
 
-    <TinyMCE
-      id={id}
-      className="PyramidFormControl"
-      onChange={(event) => { onChange(event.target.getContent(), field.name); }}
-      content={data}
-      config={getTinyMCEConfig}
-    />
-  </div>
-);
+    return true;
+  }
+
+  render() {
+    const { data, field, onChange, id } = this.props;
+
+    return (
+      <div className="PyramidBlock__ContentGroup">
+        <label className="PyramidLabel" htmlFor={id}>
+          {field.label}
+        </label>
+
+        <TinyMCE
+          id={id}
+          className="PyramidFormControl"
+          onChange={(event) => { onChange(event.target.getContent(), field.name); }}
+          content={data}
+          config={getTinyMCEConfig}
+        />
+      </div>
+    );
+  }
+}
 
 Wysiwyg.propTypes = {
   onChange: PropTypes.func.isRequired,
